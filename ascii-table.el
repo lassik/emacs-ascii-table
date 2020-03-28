27997645cac2c8218d08188d5a5bc1fb6a1e632b
Lassi Kortela <lassi@lassi.io>
2020-03-28 10:38:50 +0200

    Rename package: ascii -> ascii-table
    Recommended by multiple people in MELPA review.

--- ascii-table.el
+++ ascii-table.el
@@ -1,8 +1,8 @@
-;;; ascii.el --- Interactive ASCII table -*- lexical-binding: t -*-
+;;; ascii-table.el --- Interactive ASCII table -*- lexical-binding: t -*-
 ;;
 ;; SPDX-License-Identifier: ISC
 ;; Author: Lassi Kortela <lassi@lassi.io>
-;; URL: https://github.com/lassik/emacs-ascii
+;; URL: https://github.com/lassik/emacs-ascii-table
 ;; Package-Requires: ((emacs "24.3") (cl-lib "0.5"))
 ;; Package-Version: 0.1.0
 ;; Keywords: help tools
@@ -14,25 +14,25 @@
 ;; Show a character map of the ubiquitous 7-bit ASCII character set
 ;; (128 characters in total).
 ;;
-;; Do `M-x ascii` to bring up a window with the ASCII table.  Press 'b'
+;; Do `M-x ascii-table` to bring up a window with the ASCII table.  Press 'b'
 ;; for binary, 'o' for octal, 'd' for decimal and 'x' for hexadecimal.
 ;; Press TAB to change the way control characters are shown.
 ;;
 ;;; Code:
 
-(defvar ascii-base 16
+(defvar ascii-table-base 16
   "Number base used for character codes in the ASCII table.
 
 Valid values are 2 (binary), 8 (octal), 10 (decimal), and
 16 (hex).  Another word for 'base' is 'radix'.")
 
-(defvar ascii-control nil
+(defvar ascii-table-control nil
   "Use ^A notation for control characters in the ASCII table?
 
 If non-nil, control characters use caret notation (^A .. ^?).
 Otherwise their names NUL .. DEL are shown.")
 
-(defun ascii--binary (codepoint)
+(defun ascii-table--binary (codepoint)
   "Internal helper to format CODEPOINT in binary."
   (setq codepoint (logand codepoint #x7F))
   (string (+ ?0 (logand 1 (lsh codepoint -6)))
@@ -43,13 +43,13 @@ Otherwise their names NUL .. DEL are shown.")
           (+ ?0 (logand 1 (lsh codepoint -1)))
           (+ ?0 (logand 1 (lsh codepoint -0)))))
 
-(defun ascii--control-caret (codepoint)
+(defun ascii-table--control-caret (codepoint)
   "Internal helper to format CODEPOINT in caret notation."
   (cond ((< codepoint #x20) (string ?^ (+ ?@ codepoint)))
         ((= codepoint #x7F) "^?")
         (t nil)))
 
-(defun ascii--control-name (codepoint)
+(defun ascii-table--control-name (codepoint)
   "Internal helper to get the control character name of CODEPOINT."
   (cond ((< codepoint #x20)
          (elt ["NUL" "SOH" "STX" "ETX" "EOT" "ENQ" "ACK" "BEL"
@@ -60,7 +60,7 @@ Otherwise their names NUL .. DEL are shown.")
         ((= codepoint #x7F) "DEL")
         (t nil)))
 
-(defun ascii--table (codepoints/row)
+(defun ascii-table--table (codepoints/row)
   "Internal helper to compute the cells in the ASCII table.
 
 CODEPOINTS/ROW is how many ASCII characters to show in each row.
@@ -74,14 +74,14 @@ one for the name or other representation."
     (cl-do
         ((codepoint 0 (1+ codepoint)))
         ((= codepoint codepoints) table)
-      (let* ((code (cl-ecase ascii-base
-                     (2  (ascii--binary codepoint))
+      (let* ((code (cl-ecase ascii-table-base
+                     (2  (ascii-table--binary codepoint))
                      (8  (format "%03o" codepoint))
                      (10 (format "%d" codepoint))
                      (16 (format "%02X" codepoint))))
-             (name (or (cl-ecase ascii-control
-                         ((nil) (ascii--control-name codepoint))
-                         (caret (ascii--control-caret codepoint)))
+             (name (or (cl-ecase ascii-table-control
+                         ((nil) (ascii-table--control-name codepoint))
+                         (caret (ascii-table--control-caret codepoint)))
                        (string codepoint)))
              (row  (mod codepoint rows))
              (col  (truncate codepoint rows))
@@ -89,7 +89,7 @@ one for the name or other representation."
         (aset table (+ 0 cell) code)
         (aset table (+ 1 cell) name)))))
 
-(defun ascii--column-widths (table cols)
+(defun ascii-table--column-widths (table cols)
   "Internal helper to compute column widths needed for TABLE.
 
 Assume the table is formatted using COLS columns."
@@ -102,42 +102,42 @@ Assume the table is formatted using COLS columns."
         (let ((width (length (aref table cell))))
           (aset widths col (max (aref widths col) width)))))))
 
-(defun ascii--width-limit ()
+(defun ascii-table--width-limit ()
   "Internal helper to get narrowest window width for ASCII table."
   (let ((min-width nil))
-    (let ((ascii-buffer (get-buffer "*ASCII*")))
-      (when ascii-buffer
+    (let ((ascii-table-buffer (get-buffer "*ASCII*")))
+      (when ascii-table-buffer
         (walk-windows
          (lambda (w)
-           (when (eq ascii-buffer (window-buffer w))
+           (when (eq ascii-table-buffer (window-buffer w))
              (let ((width (window-width w)))
                (setq min-width (or (and min-width (min min-width width))
                                    width)))))
          nil t)))
     (or min-width (window-width))))
 
-(defun ascii--revert (&optional _arg _noconfirm)
+(defun ascii-table--revert (&optional _arg _noconfirm)
   "Redisplay the *ASCII* buffer, i.e. refresh the ASCII table."
   (let ((inhibit-read-only t))
-    (cl-assert (equal major-mode 'ascii-mode))
+    (cl-assert (equal major-mode 'ascii-table-mode))
     (cl-assert (null (buffer-file-name)))
     (erase-buffer)
     (insert "ASCII Table"
             " ("
-            (cl-ecase ascii-base
+            (cl-ecase ascii-table-base
               (2 "binary")
               (8 "octal")
               (10 "decimal")
               (16 "hex"))
             ")\n\n")
     (cl-dolist (codepoints/row '(8 7 6 5 4 3 2 1))
-      (let* ((table (ascii--table codepoints/row))
+      (let* ((table (ascii-table--table codepoints/row))
              (cols (* 2 codepoints/row))
              (rows (truncate (truncate (length table) 2) cols))
-             (widths (ascii--column-widths table cols))
+             (widths (ascii-table--column-widths table cols))
              (width (+ (cl-reduce #'+ widths)
                        (* 2 (length widths))))
-             (width-limit (ascii--width-limit)))
+             (width-limit (ascii-table--width-limit)))
         (when (< width width-limit)
           (cl-dotimes (row rows)
             (cl-dotimes (col cols)
@@ -154,69 +154,69 @@ Assume the table is formatted using COLS columns."
           (cl-return))))
     (goto-char (point-min))))
 
-(defun ascii--revert-if-active ()
+(defun ascii-table--revert-if-active ()
   "Redisplay the *ASCII* buffer if it exists."
   (let ((buffer (get-buffer "*ASCII*")))
-    (when buffer (with-current-buffer buffer (ascii--revert)))))
+    (when buffer (with-current-buffer buffer (ascii-table--revert)))))
 
-(defun ascii--set-base (base)
+(defun ascii-table--set-base (base)
   "Internal helper to change the ASCII number base to BASE."
-  (setq ascii-base (cl-case base ((2 8 16) base) (t 10)))
-  (ascii--revert-if-active))
+  (setq ascii-table-base (cl-case base ((2 8 16) base) (t 10)))
+  (ascii-table--revert-if-active))
 
-(defun ascii-toggle-control ()
+(defun ascii-table-toggle-control ()
   "Toggle the way control characters are shown in the ASCII table.
 
 Changes between ^A notation and control character names."
   (interactive)
-  (setq ascii-control (if ascii-control nil 'caret))
-  (ascii--revert-if-active))
+  (setq ascii-table-control (if ascii-table-control nil 'caret))
+  (ascii-table--revert-if-active))
 
-(defun ascii-base-binary ()
+(defun ascii-table-base-binary ()
   "Switch ASCII table to binary (base 2)."
   (interactive)
-  (ascii--set-base 2))
+  (ascii-table--set-base 2))
 
-(defun ascii-base-octal ()
+(defun ascii-table-base-octal ()
   "Switch ASCII table to octal (base 8)."
   (interactive)
-  (ascii--set-base 8))
+  (ascii-table--set-base 8))
 
-(defun ascii-base-decimal ()
+(defun ascii-table-base-decimal ()
   "Switch ASCII table to decimal (base 10)."
   (interactive)
-  (ascii--set-base 10))
+  (ascii-table--set-base 10))
 
-(defun ascii-base-hex ()
+(defun ascii-table-base-hex ()
   "Switch ASCII table to hexadecimal (base 16)."
   (interactive)
-  (ascii--set-base 16))
+  (ascii-table--set-base 16))
 
-(defvar ascii-mode-map
+(defvar ascii-table-mode-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map special-mode-map)
-    (define-key map (kbd "b") 'ascii-base-binary)
-    (define-key map (kbd "d") 'ascii-base-decimal)
-    (define-key map (kbd "o") 'ascii-base-octal)
-    (define-key map (kbd "x") 'ascii-base-hex)
-    (define-key map [tab] 'ascii-toggle-control)
+    (define-key map (kbd "b") 'ascii-table-base-binary)
+    (define-key map (kbd "d") 'ascii-table-base-decimal)
+    (define-key map (kbd "o") 'ascii-table-base-octal)
+    (define-key map (kbd "x") 'ascii-table-base-hex)
+    (define-key map [tab] 'ascii-table-toggle-control)
     map)
-  "Keymap for `ascii-mode'.")
+  "Keymap for `ascii-table-mode'.")
 
-(define-derived-mode ascii-mode special-mode "ASCII"
+(define-derived-mode ascii-table-mode special-mode "ASCII"
   "Major mode that shows an interactive ASCII table.
 
-\\{ascii-mode-map}"
-  (setq-local revert-buffer-function 'ascii--revert)
-  (ascii--revert))
+\\{ascii-table-mode-map}"
+  (setq-local revert-buffer-function 'ascii-table--revert)
+  (ascii-table--revert))
 
 ;;;###autoload
-(defun ascii ()
+(defun ascii-table ()
   "Show an interactive ASCII table in the other window."
   (interactive)
   (switch-to-buffer-other-window (get-buffer-create "*ASCII*"))
-  (ascii-mode))
+  (ascii-table-mode))
 
-(provide 'ascii)
+(provide 'ascii-table)
 
-;;; ascii.el ends here
+;;; ascii-table.el ends here
