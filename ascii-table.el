dc3c91feff6282303b66816bdcee9e031558ff77
Lassi Kortela <lassi@lassi.io>
2023-12-15 17:27:30 +0200

    Turn blank comment lines into blank lines

--- ascii-table.el
+++ ascii-table.el
@@ -1,23 +1,23 @@
 ;;; ascii-table.el --- Interactive ASCII table -*- lexical-binding: t -*-
-;;
+
 ;; SPDX-License-Identifier: ISC
 ;; Author: Lassi Kortela <lassi@lassi.io>
 ;; URL: https://github.com/lassik/emacs-ascii-table
 ;; Package-Requires: ((emacs "24.3"))
 ;; Version: 0.1.0
 ;; Keywords: help tools
-;;
+
 ;; This file is not part of GNU Emacs.
-;;
+
 ;;; Commentary:
-;;
+
 ;; Show a character map of the ubiquitous 7-bit ASCII character set
 ;; (128 characters in total).
-;;
+
 ;; Do `M-x ascii-table` to bring up a window with the ASCII table.  Press 'b'
 ;; for binary, 'o' for octal, 'd' for decimal and 'x' for hexadecimal.
 ;; Press TAB to change the way control characters are shown.
-;;
+
 ;;; Code:
 
 (require 'cl-lib)
